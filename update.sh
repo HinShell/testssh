5a93edb4af16f4a43ef5dc2d37797cf0df285cc2
HinShell <humanoidinshell@gmail.com>
2021-11-28 20:02:48 +0100

    second commit

--- update.sh
+++ update.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 # comment
+self=`basename "$0"`
+#echo $self
+#a=$(git diff origin/main --quiet $self)
+#echo $?
+function mytest {
+    $($@)
+    local status=$?
+    if (( status != 0 )); then
+        echo "error with $1" >&2
+    fi
+    return $status
+}
+mytest "git diff origin/main --quiet $self"
